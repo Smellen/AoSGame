2e20aa02f9d17cbb01441653e3157f29d61b2f81
Smellen <eburke624@gmail.com>
2015-09-13 18:00:53 +0100

    Adding in basic player controls.

---
+++ AoS/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerControls : MonoBehaviour
+{
+
+    /// <summary>
+    /// Player Handling
+    /// </summary>
+
+    public float gravity = 20;
+
+    public float speed = 10;
+    public float acceleration = 40;
+    public float jumpHeight = 12;
+
+    public float currentSpeed;
+    public float targetSpeed;
+
+
+    private Vector2 amountToMove;
+
+    private PlayerPhysics playerPhysics;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerPhysics = GetComponent<PlayerPhysics>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
+        currentSpeed = Towards(currentSpeed, targetSpeed, acceleration);
+
+        //// If player is on the ground
+        if (playerPhysics.grounded)
+        {
+            amountToMove.y = 0;
+            if (Input.GetButtonDown("Jump"))
+            {
+                amountToMove.y = jumpHeight;
+            }
+        }
+
+        amountToMove.x = currentSpeed;
+        amountToMove.y -= gravity * Time.deltaTime;
+        playerPhysics.Move(amountToMove * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Increase n towards target by speed.
+    /// </summary>
+    /// <param name="n">Current speed.</param>
+    /// <param name="target">The target.</param>
+    /// <param name="howFastIncrementing">The speed.</param>
+    /// <returns></returns>
+    private float Towards(float n, float target, float howFastIncrementing)
+    {
+        if (n == target)
+        {
+            return n;
+        }
+        else
+        {
+            var dir = Mathf.Sign(target - n);
+            n += howFastIncrementing * Time.deltaTime * dir;
+            return (dir == Mathf.Sign(target - n)) ? n : target;
+        }
+
+    }
+}
---
+++ AoS/Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+public class PlayerPhysics : MonoBehaviour
+{
+
+    public LayerMask collisionMask;
+
+    private BoxCollider collider;
+
+    private Vector3 size;
+    private Vector3 centre;
+
+    private float skin = .005f;
+
+    Ray ray;
+    RaycastHit hit;
+
+    [HideInInspector]
+    public bool grounded;
+
+    void Start()
+    {
+        collider = GetComponent<BoxCollider>();
+        size = collider.size;
+        centre = collider.center;
+    }
+
+    /// <summary>
+    /// A method to move the player by a certain amount.
+    /// </summary>
+    /// <param name="moveAmount">The amount to move.</param>
+    public void Move(Vector2 moveAmount)
+    {
+        var deltaY = moveAmount.y;
+        var deltaX = moveAmount.x;
+
+        Vector2 playerPos = transform.position;
+
+        grounded = false;
+        for (var i = 0; i < 3; i++)
+        {
+            var dir = Mathf.Sign(deltaY);
+            var x = (playerPos.x + centre.x - size.x / 2) + size.x / 2 * i;
+            var y = playerPos.y + centre.y + size.y / 2 * dir;
+
+            ray = new Ray(new Vector2(x, y), new Vector2(0, dir));
+
+            Debug.DrawRay(ray.origin, ray.direction);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Abs(deltaY), collisionMask))
+            {
+                //// Distance between the player and the ground.
+                var dst = Vector3.Distance(ray.origin, hit.point);
+
+                //// Stop downward movement after meeting the collider.
+                if (dst > skin)
+                {
+                    ////deltaY = dst * dir + skin;
+                    deltaY = -dst + skin;
+                }
+                else
+                {
+                    deltaY = 0;
+                }
+
+                grounded = true;
+                break;
+            }
+        }
+
+        var newPostion = new Vector2(deltaX, deltaY);
+        transform.Translate(newPostion);
+    }
+}
